007160bfe8202e66d6826ad4bcb27c9402ba54a2
irfanmhd2006-afk <irfanmhd2006@gmail.com>
2025-09-27 00:41:49 +0700

    Add files via upload

---
+++ prakalpro_2025_D_2511531008/src/Pekan2/TugasAlproPekan2.java
@@ -0,0 +1,16 @@
+package Tugas_Prakalpro;
+public class TugasAlproPekan2 {
+    public static void main(String[] args) {
+        boolean target = true;              // sudah tercapai atau belum
+        char pencapaian = 'A';              // kategori pencapaian (A = Sangat Baik)
+        float jarakTempuh = 5.7f;           // dalam kilometer
+        int jumlahKalori = 450;             // kalori terbakar
+
+        System.out.println("===== DATA JOGGING =====");
+        System.out.println("Target Tercapai   : " + target);
+        System.out.println("Pencapaian        : " + pencapaian);
+        System.out.println("Jarak Tempuh (km) : " + jarakTempuh);
+        System.out.println("Jumlah Kalori     : " + jumlahKalori);
+    }
+}
+
