51e9a7c48bd3d2c56648ab5ca4794c6922695744
irfanmhd2006-afk <irfanmhd2006@gmail.com>
2025-10-31 09:42:38 +0700

    Add files via upload

---
+++ prakalpro_2025_D_2511531008/src/pekan5/BelahketupatWOI.java
@@ -0,0 +1,61 @@
+package tugasprakalpro;
+
+
+public class BelahketupatWOI {
+	
+	static void cover(){
+		int n=9;
+		System.out.print("#");
+	    for (int i = 1; i < 2 * n-1; i++) System.out.print("=");
+	    System.out.println("#");
+	}
+    public static void main(String[] args) {
+        int n = 4; // tinggi setengah belah ketupat
+
+        // Bagian atas bingkai
+        cover();
+
+        // Bagian isi (belah ketupat)
+        for (int i = 1; i <= n; i++) {
+        	//garis tepi kiri
+            System.out.print("|");
+            
+            for(int j = i; j < n; j++) {
+            	System.out.print("  ");
+            }
+            System.out.print("<>");
+            for(int k = 1; k < i; k++) {
+            	System.out.print("....");
+            }
+            System.out.print("<>");
+            for(int j = i; j < n; j++) {
+            	System.out.print("  ");
+            }
+            System.out.println("|");
+        }
+        
+        for (int i = 1; i <= n; i++) {
+        	//garis tepi kiri
+            System.out.print("|");
+            
+            for(int j = 1; j < i ; j++) {
+            	System.out.print("  ");
+            }
+            System.out.print("<>");
+            for(int k = 1; k < n-i+1; k++) {
+            	System.out.print("....");
+            }
+            System.out.print("<>");
+            for(int j = 1; j < i ; j++) {
+            	System.out.print("  ");
+            }
+            System.out.println("|");
+        }
+
+        
+
+        
+        cover();
+    }
+}
+
