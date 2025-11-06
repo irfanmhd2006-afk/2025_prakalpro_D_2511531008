48eb2d0b0f6f3ac65ca334dbf330d303a7ce9dcf
irfanmhd2006-afk <irfanmhd2006@gmail.com>
2025-11-06 10:42:38 +0700

    Add files via upload

---
+++ prakalpro_2025_D_2511531008/src/Pekan6_2511531008/GameGacha_2511531008.java
@@ -0,0 +1,46 @@
+package tugasprakalpro;
+import java.util.Scanner;
+import java.util.Random;
+
+public class GameGacha_2511531008 {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		boolean cek = true, menang = true;
+		int gacha = 0;
+		String jawab = null;
+		
+		while(cek) {
+			int sum1 = rand.nextInt(6) + 1;
+			int sum2 = rand.nextInt(6) + 1;
+			int hasil = sum1 + sum2;
+			gacha++;
+			System.out.println(sum1 +" + "+ sum2 +" = " + hasil );
+			if( hasil == 7) {
+				System.out.println("Tebakan Anda benar");
+				cek = false;
+				
+			}else {
+				System.out.println("Tebakan Anda Salah");
+				System.out.print("Apakah mau lempar dadu (iya / tidak?)");
+				jawab = scan.nextLine();
+			}
+			
+			if(jawab.equalsIgnoreCase("tidak")){
+				menang = false;
+				cek = false;
+			}
+			
+		}
+		
+		if(menang) System.out.println("Anda menang setelah " + gacha +" percobaan!!");
+		else System.out.println("Anda Gagal menang");
+		
+		scan.close();
+		
+		
+
+	}
+
+}
